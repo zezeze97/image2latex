5a27a8d4cd170fceeb0ec874c7066ae9adee711f
zezeze97 <18428308691@163.com>
2022-01-05 22:52:01 +0800

    fix

--- im2latex/im2latex_res31_infer.sh
+++ im2latex/im2latex_res31_infer.sh
@@ -1,1 +1,1 @@
-tools/ocr_test_imgs.sh expr_result/im2latex_res31/master_Resnet31_withGCB_im2latex.py expr_result/im2latex_res31/pretrained.pth /home/zhangzr/im2latex_data/formula_images_processed /home/zhangzr/im2latex_data/master_data/test.txt expr_result/im2latex_res31/predict/
+tools/ocr_test_imgs.sh configs/textrecog/master/master_Resnet31_withGCB_im2latex.py expr_result/im2latex_res31/pretrained.pth /home/zhangzr/im2latex_data/formula_images_processed /home/zhangzr/im2latex_data/master_data/test.txt expr_result/im2latex_res31/predict/
