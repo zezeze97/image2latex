b3fa2bea5ae120c034484b626ab9884693fc05e6
zezeze97 <18428398691@163.com>
2022-01-19 17:26:17 +0800

    fix

--- im2latex/post_process.py
+++ im2latex/post_process.py
@@ -1,25 +1,22 @@
 
-def minRemoveToMakeValid(latex,left_symbel,right_symbel):
-    temp_stack = []
-    i = 0
-    N = len(latex)
-    while i < N:
-        if latex[i] == right_symbel:
-            if len(temp_stack) != 0:
-                latex = latex[:i] + latex[i+1:]
-                N -= 1
-                i -= 1
-            else:
-                temp_stack.pop()
-        elif latex[i] == left_symbel:
-            temp_stack.append(i)
-    while len(temp_stack) != 0:
-        i = temp_stack.pop()
-        latex = latex[:i] + latex[i+1:]
+def minRemoveToMakeValid(latex,left_symble,right_symble):
+    left_num = 0
+    right_num = 0
+    for token in latex:
+        if token == left_symble:
+            left_num += 1
+        if token == right_symble:
+            right_num += 1
+    if left_num > right_num:
+        latex += right_symble*(left_num - right_num)
+    if right_num > left_num:
+        latex = left_symble*(right_num - left_num) + latex
     return latex
+    
 
 
 if __name__ == '__main__':
-    raw_latex = '/alpha_{1'
+    raw_latex = r'X_}'
     prcessed_latex = minRemoveToMakeValid(raw_latex,'{','}')
+    prcessed_latex = minRemoveToMakeValid(prcessed_latex,'(',')')
     print(prcessed_latex)
